cecb1ddd401239a840d102c2f2780510d350d12b
shajn786 <shajn786@gmail.com>
2022-11-25 14:54:52 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductViewAllComponent } from './product-view-all/product-view-all.component';
+
+const myRoutes: Routes=
+[
+  {
+    path:"",component:ProductAddComponent
+  },
+  {
+    path:"viewall",component:ProductViewAllComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +25,8 @@ import { ProductViewAllComponent } from './product-view-all/product-view-all.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
