670b1d0d23ed6d446c7a1b1a71d4e3ee11c40cb9
shajn786 <shajn786@gmail.com>
2022-11-25 14:35:00 +0530

    view all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductViewAllComponent } from './product-view-all/product-view-all.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductAddComponent
+    ProductAddComponent,
+    ProductViewAllComponent
   ],
   imports: [
     BrowserModule,
